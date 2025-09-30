17f9143f9c564347a2b7080ba25f45b50560543a
Alsaya <emmagddc@hotmail.com>
2025-09-30 13:22:37 +0200

    pwet

--- index.php
+++ index.php
@@ -1,14 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="NebulaSync vous accompagne dans la création de sites web modernes, accessibles et sur mesure. Sites vitrines, portfolios, blogs – donnez vie à votre présence en ligne dès aujourd'hui.">
+    <meta name="author" content="NebulaSync">
+    <meta name="copyright" content="NebulaSync">
+    <meta name="robots" content="index, follow">
+
+    <!-- Open Graph (Facebook, LinkedIn, WhatsApp, etc) -->
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="fr_FR">
+    <meta property="og:site_name" content="Nebula Sync">
+    <meta property="og:title" content="Nebula Sync">
+    <meta property="og:description" content="NebulaSync vous accompagne dans la création de sites web modernes, accessibles et sur mesure. Sites vitrines, portfolios, blogs – donnez vie à votre présence en ligne dès aujourd'hui.">
+    <meta property="og:url" content="https://votre-domaine.fr/">
+    <meta property="og:image" content="assets/image/logo.png">
+
+    <!-- Carte Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="NebulaSync">
+    <meta name="twitter:description" content="NebulaSync vous accompagne dans la création de sites web modernes, accessibles et sur mesure. Sites vitrines, portfolios, blogs – donnez vie à votre présence en ligne dès aujourd'hui.">
+    <meta name="twitter:image" content="profil.jpg">
+
+    <title>Formulaire - ECF</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Goldman:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets/image/logo.png" type="image/x-icon">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
 </head>
 <body>
     
 
 
 
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
